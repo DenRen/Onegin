ca202c0e92ba202c6008a8c399c04c2ab01218f7
DenRen <denrenruslan@gmail.com>
2019-09-17 18:30:25 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Onegin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Onegin.dir/main.cpp.o"
+  "Onegin.pdb"
+  "Onegin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Onegin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
