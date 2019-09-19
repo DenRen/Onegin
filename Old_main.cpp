c093e329f6a44f44368c79c702c5a52e05f2dbfe
DenRen <denrenruslan@gmail.com>
2019-09-20 00:05:16 +0300

    Начал полностью переписывать код. Подробности в описании
    1) Будет полность изменена система хранения и обработки текста в памяти
    2) Сделал разделения для UNIX и других ОС, при узнавании размера файла.

---
+++ Old_main.cpp
@@ -0,0 +1,283 @@
+#include <iostream>
+#include <cassert>
+
+const int MAXSIZE = 200;
+const int MINLINE = 20;
+
+struct struct_pS {
+    char *p;
+    unsigned l;
+};
+
+unsigned Symbol_in_file(const char name[], unsigned *x);/*
+ * Доп. семинар в пятницу
+ * Timoxa511
+ * */
+
+unsigned getline(char *s, FILE *input);
+
+bool is_numb_letter(char c);
+
+void str_cpy(char *s1, char * s2);
+
+int comparison_str(struct_pS structPS_1, struct_pS structPS_2);
+
+int comparison_str_rev(struct_pS structPS_1, struct_pS structPS_2);
+
+char lower(char c);
+
+int comp1(const void *a, const void *b) {
+    return comparison_str(*(struct_pS *) a, *(struct_pS *) b);
+}
+
+int comp2(const void *a, const void *b) {
+    return comparison_str_rev(*(struct_pS *) a, *(struct_pS *) b);
+}
+
+int main() {
+    // Ввожу имена файлов ввода и вывода спеллчекер
+    const char name[] = "INPUT.txt", name_out[] = "OUTPUT.txt";
+
+
+
+    //Считаю кол-во символов в файле (тип getline()) и кол-во строк
+    unsigned number_s = 0;
+    unsigned size_array = Symbol_in_file(name, &number_s);
+    if (size_array == 0) {
+        printf("%s", "\n" "main ERROR: size_array = 0\n");
+        return 0;
+    }
+
+    // Заполняю массив S и запоминаю в pS указатели на начало предложений
+    char *S = (char *) calloc(size_array, sizeof(char **)), s[MAXSIZE];
+    struct_pS pS[number_s];
+
+    unsigned i, j, length = 0; // Длина текущего предложения
+    FILE *F = fopen(name, "r");
+    if (F == nullptr) {
+        printf("Error open file %s\n", name);
+        fclose(F);
+        return 0;
+    }
+    for (int i = 0, j = 0; (length = getline(s, F)) != 0; i += length, j++) {
+        str_cpy(&S[i], s);
+        pS[j].p = &S[i++];
+        pS[j].l = length;
+    }
+    fclose(F);
+
+    FILE *O = fopen(name_out, "w");
+    if (O == nullptr) {
+        printf("\n%s%s\n", "main ERROR: no open file ", name_out);
+        fclose(O);
+        free(S);
+        return 0;
+    }
+
+    // Сортирую слева и записываю в O
+    qsort(pS, number_s, sizeof(struct_pS), comp1);
+    for (i = 0; i < number_s; i++)
+        fprintf(O, "%s\n", pS[i].p);
+    fprintf(O, "\n%s\n", "--------------------------------------------------");
+
+    // Сортирую справа и записываю в O
+    qsort(pS, number_s, sizeof(struct_pS), comp2);
+    for (i = 0; i < number_s; i++)
+        fprintf(O, "%s\n", pS[i].p);
+    fprintf(O, "\n%s\n", "--------------------------------------------------");
+
+    for (j = i = 0; i < number_s;) {
+        if (S[j] != '\0')
+            fprintf(O, "%c", S[j++]);
+        else {
+            i++, j++;
+            fprintf(O, "%c", '\n');
+        }
+    }
+    char temp = '{';
+
+    printf("Hello. Want to get a super verse? y/n");
+    scanf("%c", &temp);
+    if (temp == 'y') {
+        printf("\nHow many lines?\n");
+        unsigned line = 0;
+        if (scanf("%d", &line) == 1) {
+            if (line > number_s)
+                line = number_s;
+            srand(time(nullptr));
+
+            int random = rand() % (number_s);
+
+            unsigned left = random - line / 2, right = random + line / 2;
+            if (left < 0)
+                left = 0;
+            if (right > number_s)
+                right = number_s;
+
+            for (i = left; i < right + 1; i++)
+                printf("%s\n", pS[i].p);
+
+        } else {
+            printf("I don’t understand you. Bye see you later\n");
+        }
+    } else {
+        printf("Okey :(\n");
+    }
+
+    free(S);
+    return 0;
+}
+
+char lower(char c) {
+    if (c >= 'A' && c <= 'Z')
+        return c + 'a' - 'A';
+    return c;
+}
+
+bool is_punct_mark(unsigned c) {
+    //Проверка на знаки препинания и не букв-симв сиволы
+    return c == ' ' || c == '\"' || c == '.' || c == '!' ||
+           c == '\?' || c == '-' || c == '\'';
+}
+
+bool is_numb_letter(char c) {
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+}
+
+bool is_letter(char c) {
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
+
+bool is_strock_liter(unsigned c) {
+    return c == '\t' || c == '\n' || c == '\v';
+}
+
+unsigned Symbol_in_file(char name[], unsigned *x) {
+    // Возвращает длину предложения с учётом '\0'
+    // Специально для формата getline()
+    assert(x != nullptr);
+
+    FILE *input = fopen(name, "r");
+    if (input == nullptr) {
+        printf("Symbol_in_file: Error open file %s\n", name);
+        fclose(input);
+        return 0;
+    }
+    unsigned c = ' ', i = MINLINE - 1, n = 0, j = 0;
+
+    // Пропускаем безинформативные строки
+    while (i != 0) {
+        i = 0;
+        while (i < MINLINE && c != EOF) {
+            i = 0;
+            while ((c = getc(input)) != EOF && (is_strock_liter(c) || c == ' '));
+
+            do {
+                if (is_strock_liter(c)) {
+                    break;
+                } else {
+                    i++;
+                }
+            } while ((c = getc(input)) != EOF && c != '\n');
+        }
+        n += i;
+        if (i != 0)
+            j++;
+    }
+    *x = j - 1;
+    fclose(input);
+    return n;
+}
+
+unsigned getline(char *s, FILE *input = stdin) {
+    // Без проверки потока (контракт)
+    // Возвращает длину предложения без учёта '\0'
+    // И записывает считанную строку в s
+    // Чтобы не запоминать строки, которые начинаются с цифры
+    // нужно для проверок установить какой-нибудь первый символ
+    s[0] = '}';
+    unsigned c = ' ', i = 0;
+    // Пропускаем безинформативные строки
+    while (i < MINLINE && c != EOF) {
+        i = 0;
+        while ((c = getc(input)) != EOF && (is_strock_liter(c) || c == ' '));
+        // Читаем и записываем строку полностью
+        /* Можно было написать и оптимизированнее, но
+         * хотел как можно подробнее
+         */
+        do {
+            if (is_strock_liter(c)) {
+                break;
+            } else {
+                s[i++] = (char) c;
+            }
+        } while ((c = getc(input)) != EOF && c != '\n');
+        s[i] = '\0';
+    }
+    if (i < MINLINE)
+        return 0;
+    return i;
+}
+
+void str_cpy(char *s1, char *s2) {
+    while ((*(s1)++ = *(s2)++) != '\0')
+        ;
+}
+
+//! This function comparison of string to the left
+//! @param [in] structPS_1
+//! @param [in] structPS_2
+//!
+//! @return Left and right row difference
+
+int comparison_str(struct_pS structPS_1, struct_pS structPS_2) {
+    // Считается, что "aaa" < "aaaa"
+    char *s1 = structPS_1.p, *s2 = structPS_2.p;
+    while (true) {
+        while (!is_numb_letter(*s1) && *s1 != '\0')
+            s1++;
+        while (!is_numb_letter(*s2) && *s2 != '\0')
+            s2++;
+
+        if (*s1 == '\0' && *s2 != '\0')
+            return -1;
+        else if (*s1 != '\0' && *s2 == '\0')
+            return 1;
+        else if (*s1 == '\0' && *s2 == '\0')
+            return 0;
+
+        if (lower(*s1) != lower(*s2))
+            return (lower(*s1) - lower(*s2));
+        s1++, s2++;
+    }
+}
+
+//! This function comparison of string to the right
+//! @param [in] structPS_1
+//! @param [in] structPS_2
+//!
+//! @return Right and left row difference
+
+int comparison_str_rev(struct_pS structPS_1, struct_pS structPS_2) {
+    // Считается, что "aaa" < "aaaa"
+    char *s1 = structPS_1.p + structPS_1.l - 1;
+    char *s2 = structPS_2.p + structPS_2.l - 1;
+
+    while (true) {
+        while (!is_numb_letter(*s1) && *s1 != '\0')
+            s1--;
+        while (!is_numb_letter(*s2) && *s2 != '\0')
+            s2--;
+
+        if (*s1 == '\0' && *s2 != '\0')
+            return -1;
+        else if (*s1 != '\0' && *s2 == '\0')
+            return 1;
+        else if (*s1 == '\0' && *s2 == '\0')
+            return 0;
+
+        if (lower(*s1) != lower(*s2))
+            return (lower(*s1) - lower(*s2));
+        s1--, s2--;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,126 +1,63 @@
 #include <iostream>
-#include <cassert>
+#include <unistd.h>
 
-#define MAXSIZE 200
-#define MINLINE 20
+const int MAXSIZE = 200;
+const int MINLINE = 20;
 
+char *Read_File_To_Buffer(const char *name, int *state_func, bool UNIX);
 
-struct struct_pS {
-    char *p;
-    unsigned l;
-};
-
-unsigned Symbol_in_file(char name[], unsigned *x);
-
-unsigned getline(char *s, FILE *input);
-
-bool is_numb_letter(char c);
-
-void str_cpy(char *s1, char *s2);
-
-int comparison_str(struct_pS structPS_1, struct_pS structPS_2);
-
-int comparison_str_rev(struct_pS structPS_1, struct_pS structPS_2);
-
-char lower(char c);
-
-int comp1(const void *a, const void *b) {
-    return comparison_str(*(struct_pS *) a, *(struct_pS *) b);
-}
-
-int comp2(const void *a, const void *b) {
-    return comparison_str_rev(*(struct_pS *) a, *(struct_pS *) b);
-}
+int Converter_for_Verse(char *buf, char **pointer_to_text);
 
 int main() {
-    // Воожу имена файлов ввода и вывода
-    char name[] = "INPUT.txt", name_out[] = "OUTPUT.txt";
+    const char temp_name[] = "Test_File.txt";
+    int state = 0;
+    char *buf1 = Read_File_To_Buffer(temp_name, &state, true);
 
-    //Считаю кол-во символов в файле (тип getline()) и кол-во строк
-    unsigned number_s = 0;
-    unsigned size_array = Symbol_in_file(name, &number_s);
-    if (size_array == 0) {
-        printf("%s", "\nmain ERROR: size_array = 0\n");
-        return 0;
-    }
 
-    // Заполняю массив S и запоминаю в pS указатели на начало предложений
-    char *S = (char *) calloc(size_array, sizeof(char **)), s[MAXSIZE];
-    struct_pS pS[number_s];
+    return 0;
+}
 
-    unsigned i, j, length = 0; // Длина текущего предложения
-    FILE *F = fopen(name, "r");
-    if (F == nullptr) {
-        printf("Error open file %s\n", name);
-        fclose(F);
-        return 0;
-    }
-    for (i = j = 0; (length = getline(s, F)) != 0; i += length, j++) {
-        str_cpy(&S[i], s);
-        pS[j].p = &S[i++];
-        pS[j].l = length;
-    }
-    fclose(F);
+char *Read_File_To_Buffer(const char *name, int *state_func, bool UNIX) {
+    // Сам очистит буффер при ошибке
+    // --------------------------------------------------------------------
+    // В системе UNIX можно сделать проверку на существование
+    // и на доступность чтения данного файла и при помощи fseek
+    // и ftell узнать размер файла (Переменной UNIX передать true).
+    // Но для кроссплатформенности нужно использовать fstat (UNIX == false)
+    // --------------------------------------------------------------------
 
-    FILE *O = fopen(name_out, "w");
-    if (O == nullptr) {
-        printf("\n%s%s\n", "main ERROR: no open file ", name_out);
-        fclose(O);
-        free(S);
-        return 0;
-    }
-
-    // Сортирую слева и записываю в O
-    qsort(pS, number_s, sizeof(struct_pS), comp1);
-    for (i = 0; i < number_s; i++)
-        fprintf(O, "%s\n", pS[i].p);
-    fprintf(O, "\n%s\n", "--------------------------------------------------");
-
-    // Сортирую справа и записываю в O
-    qsort(pS, number_s, sizeof(struct_pS), comp2);
-    for (i = 0; i < number_s; i++)
-        fprintf(O, "%s\n", pS[i].p);
-    fprintf(O, "\n%s\n", "--------------------------------------------------");
-
-    for (j = i = 0; i < number_s;) {
-        if (S[j] != '\0')
-            fprintf(O, "%c", S[j++]);
-        else {
-            i++, j++;
-            fprintf(O, "%c", '\n');
-        }
-    }
-    char temp = '{';
-
-    printf("Hello. Want to get a super verse? y/n");
-    scanf("%c", &temp);
-    if (temp == 'y') {
-        printf("\nHow many lines?\n");
-        unsigned line = 0;
-        if (scanf("%d", &line) == 1) {
-            if (line > number_s)
-                line = number_s;
-            srand(time(nullptr));
-
-            int random = rand() % (number_s);
-
-            unsigned left = random - line / 2, right = random + line / 2;
-            if (left < 0)
-                left = 0;
-            if (right > number_s)
-                right = number_s;
-
-            for (i = left; i < right + 1; i++)
-                printf("%s\n", pS[i].p);
-
-        } else {
-            printf("I don’t understand you. Bye see you later\n");
+    FILE *file = nullptr;
+    unsigned long file_size = 0;
+    if (UNIX) { // Узнаём размер буду в ОС на UNIX
+        if (!access(name, 3) || !access(name, 4)) {  // Проверим доступность файла
+            if ((file = fopen(name, "r")) != nullptr) {
+                fseek(file, 0, SEEK_END);
+                file_size = ftell(file);
+                fseek(file, 0, SEEK_SET);
+                printf("file size: %lu, ", file_size);
+            } else {
+                printf("\n""ERROR Read_File_To_Buffer: %s nullptr \n", name);
+            }
+        } else {    // Узнаём размер буду в ОС на Windows
+            printf("\n""ERROR Read_File_To_Buffer: "
+                   "the file %s is either not present or access is denied\n", name);
         }
     } else {
-        printf("Okey :(\n");
+        
+    }
+    printf("%c\n", getc(file));
+    if (file_size == 0) {
+        *state_func = 1;
+        return nullptr;
     }
 
-    free(S);
+    *state_func = 0;
+    return nullptr;
+}
+
+int Converter_for_Verse(char *buf, char **pointer_to_text) {
+    // При ошибке конвертирования автоматически освобождается
+    // память выделанная на pointer_text
     return 0;
 }
 
@@ -130,149 +67,27 @@ char lower(char c) {
     return c;
 }
 
-bool is_punct_mark(unsigned c) {
-    //Проверка на знаки препинания и не букв-симв сиволы
-    return c == ' ' || c == '\"' || c == '.' || c == '!' ||
-           c == '\?' || c == '-' || c == '\'';
+
+/*
+// Ввожу имена файлов ввода и вывода спеллчекер
+const char name[] = "INPUT.txt", name_out[] = "OUTPUT.txt";
+
+// Копирую файл name в беффер и проверяю ошибки
+int state_func_ReadFile = 0;
+char *buf = Read_File_To_Buffer(name, &state_func_ReadFile);
+if (buf == nullptr) {
+    printf("\n" "main: ERROR in Read_File_To_Buffer: %d\n", state_func_ReadFile);
+    return 0;
 }
 
-bool is_numb_letter(char c) {
-    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+// Делаем пригодным для сортировки текст в buf и возвращем
+// text как массив указателей на начала предложений в buf
+char *text = nullptr;
+int state_converter = Converter_for_Verse(buf, &text);
+if (state_converter == -1 && text == nullptr) {
+    printf("\n" "main: ERROR in Converter_for_Verse\n");
+    free(buf);
+    return 0;
 }
 
-bool is_letter(char c) {
-    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
-}
-
-bool is_strock_liter(unsigned c) {
-    return c == '\t' || c == '\n' || c == '\v';
-}
-
-unsigned Symbol_in_file(char name[], unsigned *x) {
-    // Возвращает длину предложения с учётом '\0'
-    // Специально для формата getline()
-    assert(x != nullptr);
-
-    FILE *input = fopen(name, "r");
-    if (input == nullptr) {
-        printf("Symbol_in_file: Error open file %s\n", name);
-        fclose(input);
-        return 0;
-    }
-    unsigned c = ' ', i = MINLINE - 1, n = 0, j = 0;
-
-    // Пропускаем безинформативные строки
-    while (i != 0) {
-        i = 0;
-        while (i < MINLINE && c != EOF) {
-            i = 0;
-            while ((c = getc(input)) != EOF && (is_strock_liter(c) || c == ' '));
-
-            do {
-                if (is_strock_liter(c)) {
-                    break;
-                } else {
-                    i++;
-                }
-            } while ((c = getc(input)) != EOF && c != '\n');
-        }
-        n += i;
-        if (i != 0)
-            j++;
-    }
-    *x = j - 1;
-    fclose(input);
-    return n;
-}
-
-unsigned getline(char *s, FILE *input = stdin) {
-    // Без проверки потока (контракт)
-    // Возвращает длину предложения без учёта '\0'
-    // И записывает считанную строку в s
-    // Чтобы не запоминать строки, которые начинаются с цифры
-    // нужно для проверок установить какой-нибудь первый символ
-    s[0] = '}';
-    unsigned c = ' ', i = 0;
-    // Пропускаем безинформативные строки
-    while (i < MINLINE && c != EOF) {
-        i = 0;
-        while ((c = getc(input)) != EOF && (is_strock_liter(c) || c == ' '));
-        // Читаем и записываем строку полностью
-        /* Можно было написать и оптимизированнее, но
-         * хотел как можно подробнее
-         */
-        do {
-            if (is_strock_liter(c)) {
-                break;
-            } else {
-                s[i++] = (char) c;
-            }
-        } while ((c = getc(input)) != EOF && c != '\n');
-        s[i] = '\0';
-    }
-    if (i < MINLINE)
-        return 0;
-    return i;
-}
-
-void str_cpy(char *s1, char *s2) {
-    while ((*(s1)++ = *(s2)++) != '\0');
-}
-
-//! This function comparison of string to the left
-//! @param [in] structPS_1
-//! @param [in] structPS_2
-//!
-//! @return Left and right row difference
-
-int comparison_str(struct_pS structPS_1, struct_pS structPS_2) {
-    // Считается, что "aaa" < "aaaa"
-    char *s1 = structPS_1.p, *s2 = structPS_2.p;
-    while (true) {
-        while (!is_numb_letter(*s1) && *s1 != '\0')
-            s1++;
-        while (!is_numb_letter(*s2) && *s2 != '\0')
-            s2++;
-
-        if (*s1 == '\0' && *s2 != '\0')
-            return -1;
-        else if (*s1 != '\0' && *s2 == '\0')
-            return 1;
-        else if (*s1 == '\0' && *s2 == '\0')
-            return 0;
-
-        if (lower(*s1) != lower(*s2))
-            return (lower(*s1) - lower(*s2));
-        s1++, s2++;
-    }
-}
-
-//! This function comparison of string to the right
-//! @param [in] structPS_1
-//! @param [in] structPS_2
-//!
-//! @return Right and left row difference
-
-int comparison_str_rev(struct_pS structPS_1, struct_pS structPS_2) {
-    // Считается, что "aaa" < "aaaa"
-    char *s1 = structPS_1.p + structPS_1.l - 1;
-    char *s2 = structPS_2.p + structPS_2.l - 1;
-
-    while (true) {
-        while (!is_numb_letter(*s1) && *s1 != '\0')
-            s1--;
-        while (!is_numb_letter(*s2) && *s2 != '\0')
-            s2--;
-
-        if (*s1 == '\0' && *s2 != '\0')
-            return -1;
-        else if (*s1 != '\0' && *s2 == '\0')
-            return 1;
-        else if (*s1 == '\0' && *s2 == '\0')
-            return 0;
-
-        if (lower(*s1) != lower(*s2))
-            return (lower(*s1) - lower(*s2));
-        s1--, s2--;
-    }
-}
+// Сортируем и fwrite - ом записываем в name_out*/
